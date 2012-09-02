fb39cfd95f71cfd51d5a9a4b70446a81bdfd6b3a
Leho Kraav <leho@kraav.com>
2012-09-02 11:38:53 -0400

    whitespace war: tabs to spaces

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,5 @@
 (function ($) {
-	$(function () {
-		// Place your administration-specific JavaScript here
-	});
+    $(function () {
+        // Place your administration-specific JavaScript here
+    });
 }(jQuery));
