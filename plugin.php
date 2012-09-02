caf11dc6131e50daaebd2a0cb032aad9f056d254
Leho Kraav <leho@kraav.com>
2012-09-02 11:37:52 -0400

    plugin metadata

--- plugin.php
+++ plugin.php
@@ -1,29 +1,13 @@
 <?php
 /*
-Plugin Name: TODO
-Plugin URI: TODO
-Description: TODO
-Version: 1.0
-Author: TODO
-Author URI: TODO
-Author Email: TODO
-License:
-
-  Copyright 2012 TODO (email@domain.com)
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License, version 2, as 
-  published by the Free Software Foundation.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-  
+Plugin Name: Simple Administrative Messages
+Plugin URI: http://markitekt.ee/wordpress/simple-administrative-messages
+Description: Let's you display helpful messages about whatever system you built to your users
+Version: 0.1
+Author: Leho Kraav
+Author URI: http://leho.kraav.com
+Author Email: leho@kraav.com
+License: GPL-2
 */
 
 // TODO: rename this class to a proper name for your plugin
