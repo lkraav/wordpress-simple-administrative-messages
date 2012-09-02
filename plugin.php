aa1d51cf4a47b9dcd27d64f4e6721ea1ea69b04c
Leho Kraav <leho@kraav.com>
2012-09-02 11:41:26 -0400

    whitespace war: kill trailing whitespace

--- plugin.php
+++ plugin.php
@@ -28,30 +28,30 @@ License:
 
 // TODO: rename this class to a proper name for your plugin
 class PluginName {
-     
+
     /*--------------------------------------------*
      * Constructor
      *--------------------------------------------*/
-    
+
     /**
      * Initializes the plugin by setting localization, filters, and administration functions.
      */
     function __construct() {
-    
+
         // TODO: replace "plugin-name-locale" with a unique value for your plugin
         load_plugin_textdomain( 'plugin-name-locale', false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
-        
+
         // Register admin styles and scripts
         add_action( 'admin_print_styles', array( &$this, 'register_admin_styles' ) );
         add_action( 'admin_enqueue_scripts', array( &$this, 'register_admin_scripts' ) );
-    
+
         // Register site styles and scripts
         add_action( 'wp_enqueue_scripts', array( &$this, 'register_plugin_styles' ) );
         add_action( 'wp_enqueue_scripts', array( &$this, 'register_plugin_scripts' ) );
-        
+
         register_activation_hook( __FILE__, array( &$this, 'activate' ) );
         register_deactivation_hook( __FILE__, array( &$this, 'deactivate' ) );
-        
+
         /*
          * TODO:
          * Define the custom functionality for your plugin. The first parameter of the
@@ -60,80 +60,80 @@ class PluginName {
          * The second parameter is the function name located within this class. See the stubs
          * later in the file.
          *
-         * For more information: 
+         * For more information:
          * http://codex.wordpress.org/Plugin_API#Hooks.2C_Actions_and_Filters
          */
         add_action( 'TODO', array( $this, 'action_method_name' ) );
         add_filter( 'TODO', array( $this, 'filter_method_name' ) );
 
     } // end constructor
-    
+
     /**
      * Fired when the plugin is activated.
      *
-     * @params    $network_wide    True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
+     * @params    $network_wide    True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog
      */
     public function activate( $network_wide ) {
         // TODO define activation functionality here
     } // end activate
-    
+
     /**
      * Fired when the plugin is deactivated.
      *
-     * @params    $network_wide    True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
+     * @params    $network_wide    True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog
      */
     public function deactivate( $network_wide ) {
-        // TODO define deactivation functionality here        
+        // TODO define deactivation functionality here
     } // end deactivate
-    
+
     /**
      * Registers and enqueues admin-specific styles.
      */
     public function register_admin_styles() {
-    
+
         // TODO change 'plugin-name' to the name of your plugin
         wp_register_style( 'plugin-name-admin-styles', plugins_url( 'plugin-name/css/admin.css' ) );
         wp_enqueue_style( 'plugin-name-admin-styles' );
-    
+
     } // end register_admin_styles
 
     /**
      * Registers and enqueues admin-specific JavaScript.
-     */    
+     */
     public function register_admin_scripts() {
-    
+
         // TODO change 'plugin-name' to the name of your plugin
         wp_register_script( 'plugin-name-admin-script', plugins_url( 'plugin-name/js/admin.js' ) );
         wp_enqueue_script( 'plugin-name-admin-script' );
-    
+
     } // end register_admin_scripts
-    
+
     /**
      * Registers and enqueues plugin-specific styles.
      */
     public function register_plugin_styles() {
-    
+
         // TODO change 'plugin-name' to the name of your plugin
         wp_register_style( 'plugin-name-plugin-styles', plugins_url( 'plugin-name/css/display.css' ) );
         wp_enqueue_style( 'plugin-name-plugin-styles' );
-    
+
     } // end register_plugin_styles
-    
+
     /**
      * Registers and enqueues plugin-specific scripts.
      */
     public function register_plugin_scripts() {
-    
+
         // TODO change 'plugin-name' to the name of your plugin
         wp_register_script( 'plugin-name-plugin-script', plugins_url( 'plugin-name/js/display.js' ) );
         wp_enqueue_script( 'plugin-name-plugin-script' );
-    
+
     } // end register_plugin_scripts
-    
+
     /*--------------------------------------------*
      * Core Functions
      *---------------------------------------------*/
-    
+
     /**
       * Note:  Actions are points in the execution of a page or process
      *        lifecycle that WordPress fires.
@@ -145,7 +145,7 @@ class PluginName {
     function action_method_name() {
         // TODO define your action method here
     } // end action_method_name
-    
+
     /**
      * Note:  Filters are points of execution in which WordPress modifies data
      *        before saving it or sending it to the browser.
@@ -157,7 +157,7 @@ class PluginName {
     function filter_method_name() {
         // TODO define your filter method here
     } // end filter_method_name
-  
+
 } // end class
 
 // TODO: update the instantiation call of your plugin to the name given at the class definition
